dab6e3a65668d3e70311f9e9ccea2619d0c09f0d
Sibabamlo@LSIBABALOM <Sibabalo.Maqiyane@redpandasoftware.co.za>
2024-08-12 10:19:11 +0200

    Updated these files

--- index.js
+++ index.js
@@ -221,4 +221,14 @@ class App {
   }
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("menu-toggle");
+  const sidebar = document.getElementById("sidebar");
+
+  menuToggle.addEventListener("click", function() {
+    sidebar.classList.toggle("collapsed");
+  });
+});
+
+
 const app = new App();
